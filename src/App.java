decd81baad39e989cd88428dfddfe478da45a398
TE24B-CENGIZ-DENIZ <deniz.cengiz@elev.ga.ntig.se>
2026-08-24 14:50:05 +0200

    uppg 3 klart

--- src/App.java
+++ src/App.java
@@ -60,28 +60,82 @@
 // }
 //number guesser
 
-import java.util.Random;
+// import java.util.Random;
+
+// void main() {
+
+//     String[] food = { "pizza", "burger", "cheese cake", "pot rost" };
+//     Random foodnum = new Random();
+//     int points = 0;
+//     int points_tried = 0;
+//     while (points_tried < 3) {
+//         int random_food = foodnum.nextInt(food.length) ;
+//         String answer = IO.readln();
+//         if (food[random_food].equals(answer)) {
+//             IO.println("correect");
+//             points = points + 1;
+//             points_tried = points_tried + 1;
+
+//         } else {
+//             IO.println("false");
+//             points_tried = points_tried + 1;
+
+//         }
+//     }
+//     IO.println("you got " + points + " out of 3");
+
+// }
 
 void main() {
     
-    String[] food = { "pizza", "burger", "cheese cake", "pot rost" };
-    Random foodnum = new Random();
-    int points = 0;
-    int points_tried = 0;
-    while (points_tried < 3) {
-        int random_food = foodnum.nextInt(food.length) ;
-        String answer = IO.readln();
-        if (food[random_food].equals(answer)) {
-            IO.println("correect");
-            points = points + 1;
-            points_tried = points_tried + 1;
+    while (true) {
+        IO.println("Meny");
+        IO.println("1. ADD");
+        IO.println("2. SUBTRACT");
+        IO.println("3. DIVIDE");
+        IO.println("4. MULTIPLY");
+        IO.println("5. STOP");
+        IO.println("pick from 1 to 5");
 
-        } else {
-            IO.println("false");
-            points_tried = points_tried + 1;
+        String answer = IO.readln("");
+        int the_answer = Integer.parseInt(answer);
+        if (the_answer == 1) {
+            IO.println("add");
+            String answer_add = IO.readln("");
+            String answer_add2 = IO.readln("");
+            int the_answer_add = Integer.parseInt(answer_add);
+            int the_answer_add2 = Integer.parseInt(answer_add2);
+            IO.println(the_answer_add + the_answer_add2);
+        } else if (the_answer == 2) {
+            IO.println("2. SUBTRACT");
+            String answer_sub = IO.readln("");
+            String answer_sub2 = IO.readln("");
+            int the_answer_sub = Integer.parseInt(answer_sub);
+            int the_answer_sub2 = Integer.parseInt(answer_sub2);
+            IO.println(the_answer_sub - the_answer_sub2);
+
+        } else if (the_answer == 3) {
+            IO.println("2. mult");
+            String answer_mult = IO.readln("");
+            String answer_mult2 = IO.readln("");
+            int the_answer_mult = Integer.parseInt(answer_mult);
+            int the_answer_mult2 = Integer.parseInt(answer_mult2);
+            IO.println(the_answer_mult * the_answer_mult2);
+
+        } else if (the_answer == 4) {
+            IO.println("2. div");
+            String answer_div = IO.readln("");
+            String answer_div2 = IO.readln("");
+            int the_answer_div = Integer.parseInt(answer_div);
+            int the_answer_div2 = Integer.parseInt(answer_div2);
+            IO.println(the_answer_div / the_answer_div2);
+
+        } else if (the_answer == 5) {
+            IO.println("2. SUBTRACT");
+            IO.println("k?");
+            break;
 
         }
-    }
-    IO.println("you got " + points + " out of 3");
 
+    }
 }
